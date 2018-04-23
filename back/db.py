efe010abcade5b4d743644d202365bc6440ca948
Robin CAVALIERI <rcavalieri@solutec.lan>
2018-04-23 14:48:33 +0200

    places ajoutées dans la BDD + ajout des reequetes INSERT

--- back/db.py
+++ back/db.py
@@ -2,12 +2,12 @@
 # coding: utf-8
 
 ###############################################################################
-#Fichier base de données
+#Fichier base de donnÃ©es
 #Par Arnaud Duhamel et Robin Cavalieri
 #Planificateur intelligent
 #SOLUTEC Paris
 #10/04/2018
-#Les fonctions d'insertion dans la BDD sont encascadées selon les dépendances
+#Les fonctions d'insertion dans la BDD sont encascadÃ©es selon les dÃ©pendances
 ###############################################################################
 
 
@@ -16,6 +16,7 @@
 ###############################################################################
 import pyodbc
 import data_mining as dm
+import numpy as np
 import pandas as pd
 import getpass
 import csv
@@ -26,7 +27,7 @@ from collections import namedtuple
 ###############################################################################
 #FONCTION
 ###############################################################################
-#Initialisation de la base de données
+#Initialisation de la base de donnÃ©es
 #Retourne le contexte
 def init_db():
     server = '10.2.38.20,1433'
@@ -37,7 +38,7 @@ def init_db():
     return connexion
 
 
-#Permet de réaliser une commande sql
+#Permet de rÃ©aliser une commande sql
 def command(db_cmd, arg):
     context=init_db()
     handler=context.cursor()
@@ -46,18 +47,17 @@ def command(db_cmd, arg):
     return res
 
 
-#Insertion des parametres dans la BDD : time |�distance |�heuristic |�position_id(FK)
+#Insertion des parametres dans la BDD : time | distance | heuristic | position_id(FK)
 def insert_param(time, distance, heuristic):
     res=command('INSERT INTO param([time],[distance],[heuristic]) VALUES(?,?,?)',(time, distance, heuristic))
     return res
 
 
-#R�cup�ration de toutes les places
+#Récupération de toutes les places
 def placesToCsv():
     temp = dm.getPlacesGps('../data/cities.csv', '../data/data_place.json')
-    #Mise au format sql
     with open('../data/all_places.csv', 'w') as myfile:
         wr = csv.writer(myfile)
         for member in temp:
-            wr.writerow(["("+member.getId(), member.getName(), member.getPhoto(), str(member.getTypes()), str(member.getGeometry()), str(member.getVisitsCount()), str(member.getCity_id())+"),"])
+            wr.writerow([member.getId(), member.getName(), member.getPhoto(), str(member.getTypes()), str(member.getGeometry()), str(member.getVisitsCount()), str(member.getCity_id())])
 ###############################################################################
