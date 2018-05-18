31b7adbc74e2d2913090ac7e8d71888c117e2ca3
Robin CAVALIERI <rcavalieri@solutec.lan>
2018-05-18 17:53:39 +0200

    travail sur la recommandation 1/2

--- back/computing.py
+++ back/computing.py
@@ -21,6 +21,8 @@ import numpy as np
 from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import monotonically_increasing_id
 from pyspark.sql.types import IntegerType
+from pyspark.sql.window import Window
+from pyspark.sql import functions as F
 import pyspark
 ###############################################################################
 
@@ -32,9 +34,9 @@ sc = pyspark.SparkContext.getOrCreate()
 conf = pyspark.SparkConf()
 conf.setAppName('SmartPlanner')
 conf.setMaster('spark://10.2.68.52:7077')
-conf.set('spark.executor.memory', '8g')
-conf.set('spark.executor.cores', '3')
-conf.set('spark.cores.max', '9')
+conf.set('spark.executor.memory', '5g')
+conf.set('spark.executor.cores', '2')
+conf.set('spark.cores.max', '6')
 conf.set('spark.logConf', True)
 sc.stop()
 sc = pyspark.SparkContext(conf=conf)
@@ -52,7 +54,7 @@ df_types=dtf.typesToDf()
 #Récupération de la matrice de similarité sous forme de dataframe
 df_similarities=dtf.similaritiesToDf()
 #Spark DataFrame
-#df_similarities=spark.createDataFrame(df_similarities)
+df_similarities=spark.createDataFrame(df_similarities)
 #Récupération de la matrice de placeTypes sous forme de dataframe
 df_placeTypes=dtf.placeTypesToDf()
 #Spark DataFrame
@@ -120,8 +122,6 @@ def getSimilarity(id_place1, id_place2):
     #Récupération du nombre de tag par id 
     nb1=len(words1)
     nb2=len(words2)
-    #print(words1)
-    #print(words2)
     nb_ind=nb1+nb2
     for i in range(0,nb1):
         for j in range(0, nb2):
@@ -132,6 +132,40 @@ def getSimilarity(id_place1, id_place2):
                 score=score+1
             #print(score)
     return score/nb_ind
+
+
+#Fonction permettant de mesurer la similarité entre les TAGS UTILISATEURS ET TOUTES LES PLACES
+def getSimilarityUsersPlaces(df_tags, df_placesTypes_ind):
+    #score=0.0
+    list_final=[]
+    for i in range(1, df_tags.count()+1):
+        for j in range(1, df_placesTypes_ind.count()+1):
+            df_similarities.show()
+            #df_placesTypes_ind.show()
+            #Select tag_id 
+            #df_tags.show()
+            tag_user=df_tags.where(df_tags.id==i).select('value').collect()
+            #Select place id
+            event_id=df_placesTypes_ind.where(df_placesTypes_ind.ind==j).select('place_id').collect()[0]
+            #Select Tags for each place
+            tags_place=df_placesTypes_ind.where(df_placesTypes_ind.place_id==event_id['place_id']).select('word')
+            #tag_user.show()
+            #tags_place.show()
+            tags_place=tags_place.withColumn('id', monotonically_increasing_id())
+            windowSpec = Window.orderBy("id")
+            tags_place=tags_place.withColumn("id", F.row_number().over(windowSpec))
+            #tags_place.show()
+            #Créer une dataframe avec la similarité de chaque event avec chaque tag
+            simi=0.0
+            score=0.0
+            for k in range (1, tags_place.count()+1):
+                temp=tags_place.where(tags_place.id==k).select('word').collect()
+                print(str(tag_user[0][0]) +" | "+ str(temp[0][0]))
+                simi=df_similarities.where(df_similarities.type_id1==tag_user[0][0] & df_similarities.type_id2==temp[0][0] ^ df_similarities.type_id2==tag_user[0][0] & df_similarities.type_id1==temp[0][0])
+                score=score+simi[0][0]
+                print(score)
+            list_final.append([tag_user, event_id['place_id'], score])
+    return 0
 
 
 #Fonction créant la matrice de similarité entre les évenements
@@ -147,6 +181,7 @@ def placesSimilarities():
             if(i!=j):
                 print(temp[i]+", "+temp[j]+" : "+str(getSimilarity(temp[i], temp[j])))
                 df_sim.loc[[temp[i]],[temp[j]]]=getSimilarity(temp[i], temp[j])
+    df_sim.to_csv('../data/placesSimilarities.csv', sep=',', encoding='utf-8')
     return(df_sim)
 
 
@@ -161,5 +196,18 @@ def computeRecommandation(tab_tags):
     #Récupération des id de tags choisis par l'utilisateur
     for i in range(0, nSize):
         user_tags.append(int(df_types.index[df_types.name==tab_tags[i]].get_values()[0]))
+    #Ajout d'index dans la dataframe des tags
+    user_tags_df=spark.createDataFrame(user_tags, IntegerType())
+    user_tags_df=user_tags_df.withColumn('id', monotonically_increasing_id())
+    windowSpec = Window.orderBy("id")
+    user_tags_df=user_tags_df.withColumn("id", F.row_number().over(windowSpec))
+    user_tags_df.cache()
+    #Ajout d'index dans la dataframe de placeTypes
+    df_placeTypes_ind=spark.createDataFrame(df_placeTypes)
+    df_placeTypes_ind=df_placeTypes_ind.withColumn('ind', monotonically_increasing_id())
+    windowSpec = Window.orderBy("ind")
+    df_placeTypes_ind=df_placeTypes_ind.withColumn("ind", F.row_number().over(windowSpec))
+    df_placeTypes_ind.cache()
+    getSimilarityUsersPlaces(user_tags_df,df_placeTypes_ind)
     return 0
 ###############################################################################
